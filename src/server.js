baaaf8131e779e4e4e390534b2ea4278b7c515ec
ManelRuano <95919095+ManelRuano@users.noreply.github.com>
2024-05-14 11:59:56 +0200

    Actualizar server.js

--- src/server.js
+++ src/server.js
@@ -2,4 +2,4 @@ const app = require('./app');
 
 // Inicialitza el servidor, que estarà escoltant en el port 3000
 const port = process.env.PORT || 8000;
-app.listen(port, () => console.log(`Pau Naharro`));
+app.listen(port, () => console.log(`Manuel Ruano`));
